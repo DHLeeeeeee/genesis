0364cb822ab7f32905cb7979ade2eac31601d193
DH <97073004+YeeDubu@users.noreply.github.com>
2022-11-29 17:51:42 +0900

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,12 @@
+$(function () {
+  $('.mainSlide').slick({
+    arrows: false,
+    dots: true,
+  });
+
+  $('.modelSlideItms').slick({
+    slidesToShow: 3,
+    arrows: false,
+    dots: true,
+  });
+});
